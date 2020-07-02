8d4b72628e44adfc05c4401c46e1ef0a652f9e44
yesid davila <elmondoles@gmail.com>
2020-07-02 01:21:54 -0500

    + grid columnas independientes + estilo cards

--- src/app/loged/components/casos/casos.component.ts
+++ src/app/loged/components/casos/casos.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+// Arrastrar y soltar.
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+// Media query.
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
 @Component({
   selector: 'app-casos',
   templateUrl: './casos.component.html',
@@ -7,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CasosComponent implements OnInit {
 
-  constructor() { }
+  panelOpenState = false;
+  columna1: any [] = [1, 2, 3, 4, 5];
+  // columna2: any [] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  constructor(private objMediaQuery: BreakpointObserver) {
+  }
 
   ngOnInit(): void {
+    this.objMediaQuery.observe([ Breakpoints.Small ]).subscribe({
+      next: (res) => {
+        console.log(res.matches);
+      },
+      error: (err) => { console.error(err.message); }
+    });
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    // Mueve un elemento de un indice en un array. // Indice seleccionado, // indice actua indicado
+    moveItemInArray(this.columna1, event.previousIndex, event.currentIndex);
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,14 +7,20 @@ import { MatButtonModule } from '@angular/material/button';   // Boton.
 import { MatDividerModule } from '@angular/material/divider'; // Barra divisora.
 import { MatSidenavModule } from '@angular/material/sidenav'; // Sidenav.
 import { MatToolbarModule } from '@angular/material/toolbar'; // toolbar Navbar
-import { MatIconModule } from '@angular/material/icon'; // Iconos de material design.
-import { MatListModule } from '@angular/material/list';   // Listado de material.
-import { MatCardModule } from '@angular/material/card'; // Card.
-import { MatGridListModule } from '@angular/material/grid-list'; // grid.
-import { MatMenuModule } from '@angular/material/menu'; // Menu
+import { MatIconModule } from '@angular/material/icon';       // Iconos de material design.
+import { MatListModule } from '@angular/material/list';       // Listado de material.
+import { MatCardModule } from '@angular/material/card';       // Card.
+import { MatGridListModule } from '@angular/material/grid-list';  // grid.
+import { MatMenuModule } from '@angular/material/menu';           // Menu.
+import { MatExpansionModule } from '@angular/material/expansion'; // Collapse.
+
+// ====== Importaciones de CDK material design
+import { CdkScrollableModule } from '@angular/cdk/scrolling'; // Contenedor con scroll
+import { DragDropModule } from '@angular/cdk/drag-drop';  // Arrastrar y soltar componentes dentro de un contenedor.
 
 // ====== Importacion de layout breakpoint (no es de materia design). (Recomendado)
 // import { FlexLayoutModule } from '@angular/flex-layout';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [],
@@ -29,7 +35,11 @@ import { MatMenuModule } from '@angular/material/menu'; // Menu
     MatListModule,
     MatCardModule,
     MatGridListModule,
-    MatMenuModule
+    MatMenuModule,
+    CdkScrollableModule,
+    DragDropModule,
+    MatExpansionModule,
+    LayoutModule
   ],
   exports: [
     MatSliderModule,
@@ -41,7 +51,11 @@ import { MatMenuModule } from '@angular/material/menu'; // Menu
     MatListModule,
     MatCardModule,
     MatGridListModule,
-    MatMenuModule
+    MatMenuModule,
+    CdkScrollableModule,
+    DragDropModule,
+    MatExpansionModule,
+    LayoutModule
   ]
 })
 export class MaterialModule { }
